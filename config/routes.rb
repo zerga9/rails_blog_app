d2bd9cb52c96f1ad8121e196b9af0d0691e02307
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-06 18:19:21 +0100

    generate articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
