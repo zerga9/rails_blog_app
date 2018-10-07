c8e536fbb3991329049959200ebfa81b239d215c
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-07 12:43:49 +0100

    Test saving article with title > 5 passing

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -14,4 +14,9 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     article = Article.create(title: 'gadi', text: 'hallohallo')
     assert_not article.save, 'Saved the article with less than 5 a title'
   end
+
+  test 'should save article with less than 5 characters title' do
+    article = Article.create(title: 'gadiza', text: 'hallohallo')
+    assert article.save, 'Not saved the article with more than 5 characters in a title'
+  end
 end
